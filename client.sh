c059a25113d21208abfbd34d0a91323649d18893
D--A--I <kanegadai@kanegadainoMacBook-Pro.local>
2019-10-27 02:04:57 +0900

    Minor modifications

--- client.sh
+++ client.sh
@@ -22,15 +22,18 @@ find ${CRAWLER_PATH}json/ -name "*.json" -mtime +3 -print | xargs rm;
 
 # `../kurumi-crawler/dist/index.js`をキックする
 echo '- kurumi-crawlerを実行します..'
-cd ${CRAWLER_PATH} && ${NODE_PATH} ./dist/index.js
+# cd ${CRAWLER_PATH} && ${NODE_PATH} ./dist/index.js
 
 if [ $? -eq 0 ]
 then
     # クローラー下の"更新日付が当日"のファイルをcurlでP0STする
     echo '- POSTを実行します..'
     for file in `find ${CRAWLER_PATH}json/ -name "*.json" -mtime 0 -print`; do
-        ${CURL_POST} ${URL} -d @${file}
+        # `curl`の標準出力は捨てる
+        ${CURL_POST} ${URL} -d @${file} >/dev/null
+        # >> `json-server`の場合"Could not resolve host: application"というエラーが出るが、一旦無視
     done
+    echo '- POSTが終了しました..'
 else
     # nodeの戻り値が不正だった場合、slackへ通知して処理終了 >> slackへの通知を別スクリプトにする
     echo '- クローリングに失敗しました。kurumi-crawler実行結果を確認してください..'
