5d75135e7c4cfd3bf089d7f9c9d60f4ee59d60d0
D--A--I <kanegadai@kanegadainoMacBook-Pro.local>
2019-10-26 23:32:52 +0900

    Implemented main function

---
+++ client.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# 開始メッセージ
+echo '- kurumi-clientを実行します..'
+
+# 現在のディレクトリの絶対パス取得
+# CURRENT_PATH=$(cd $(dirname $0) && pwd)
+
+# `kurumi-crawler`のパス
+CRAWLER_PATH=../kurumi-crawler/
+# `node`のパス
+NODE_PATH=/Users/kanegadai/.nodebrew/current/bin/node
+# POST用のcurlコマンド
+CURL_POST='/usr/bin/curl -H "Content-Type: application/json"'
+# POST先のURL
+URL=http://localhost:3000/withdrawals
+
+# クローラー下の"更新日付が3日以前"のファイルを検索
+# （xargs >> 標準入力からコマンドを作成）
+echo '- 3日以前のクローリング結果ファイルを削除します..'
+find ${CRAWLER_PATH}json/ -name "*.json" -mtime +3 -print | xargs rm;
+
+# `../kurumi-crawler/dist/index.js`をキックする
+echo '- kurumi-crawlerを実行します..'
+# cd ${CRAWLER_PATH} && ${NODE_PATH} ./dist/index.js
+
+if [ $? -eq 0 ]
+then
+    # クローラー下の"更新日付が当日"のファイルをcurlでP0STする
+    echo '- POSTを実行します..'
+    for file in `find ${CRAWLER_PATH}json/ -name "*.json" -mtime 0 -print`; do
+        ${CURL_POST} ${URL} -d @${file}
+    done
+else
+    # nodeの戻り値が不正だった場合、slackへ通知して処理終了 >> slackへの通知を別スクリプトにする
+    echo '- クローリングに失敗しました。kurumi-clawler実行結果を確認してください..'
+fi
